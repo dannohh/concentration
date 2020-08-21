3617c68994d2c2dec1faa8cf1211f7dcd918397f
Manav Misra <manavm@visionwebsoft.com>
2020-08-21 14:13:50 -0500

    :poop: Add 'addScore' api method
    Including a TODO to fix pattern.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -12,4 +12,17 @@ export default {
 
     return await cardsRes.json()
   },
+
+  // TODO: Refactor this for proper implementation of Repository API pattern!
+  async addScore(newScoreDeets) {
+    const addScoreRes = await fetch("http://localhost:5000/scores/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newScoreDeets)
+    })
+
+    return await addScoreRes.json()
+  }
 }
